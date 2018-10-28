698384d44aa51f647970ce65f1b8b18486ee8ad7
Laurentiu Spilca <laurentiu.spilca@endava.com>
2018-10-28 11:17:21 +0200

    Add changes

--- saconf2019-e2/src/main/java/com/endava/saconf2019e2/service/StatefulService.java
+++ saconf2019-e2/src/main/java/com/endava/saconf2019e2/service/StatefulService.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Service
+@RestController
 public class StatefulService {
 
     private String name;
